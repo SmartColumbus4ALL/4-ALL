13587b9cf3060e5cc78ef72a0956798578596f9c
brookehau <brookerhauser@gmail.com>
2018-05-18 23:38:55 -0400

    commit

---
+++ src/main/java/com/fourall/fourall/FourAllApplication.java
@@ -0,0 +1,12 @@
+package com.fourall.fourall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourAllApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourAllApplication.class, args);
+	}
+}
---
+++ src/test/java/com/fourall/fourall/FourAllApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fourall.fourall;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FourAllApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
